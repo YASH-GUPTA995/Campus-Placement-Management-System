9529c0b92077b57dd9d48021182bca36e0a67c8f
YASH GUPTA <engineeryash0995@gmail.com>
2026-07-14 15:24:52 +0530

    testing

--- tpo-dashboard/src/api/axios.js
+++ tpo-dashboard/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
-api.interceptors.response.use((r) => r, (err) => {
-  if (err.response?.status === 401) window.location.href = "/login";
-  return Promise.reject(err);
-});
+
 export default api;
