c9343a881f065d9543560d0f20a2cffa282d7b27
YASH GUPTA <engineeryash0995@gmail.com>
2026-07-14 16:25:11 +0530

    deployment

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-  console.log("MONGO_URI:", MONGO_URI);
+  console.log("MONGO_URL:", process.env.MONGO_URL);
+
   mongoose
-    .connect(MONGO_URI, {
+    .connect(process.env.MONGO_URL, {
       dbName: "NIT_DELHI_PLACEMENT_PORTAL",
     })
     .then(() => {
